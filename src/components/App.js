84a2e137f0d6f20eb5b9cba9d9e932167d9d7b36
joelshejar <joel.rajesh13@gmail.com>
2021-09-09 17:47:27 +0530

    Boilerplate Setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router,
+         Route,Link            
+} from 'react-router-dom'
+import Header from './Header'
+
+function App(){
+    return(
+        <>
+            <Header/>
+        </>
+    )
+}
+
+
+
