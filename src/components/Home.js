bdb1e02319595e524968346b90aae8c16ad21ae0
joelshejar <joel.rajesh13@gmail.com>
2021-09-10 09:09:12 +0530

    Home component without authorization

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Home(){
+    return(
+            <section class="flex container section-one">
+                <article class="hero">
+                    <h1>
+                        Artikel is a place where good ideas find you
+                    </h1>
+                    <p>
+                    It's easy and free to post your thinking on any topic and connect with millions of readers.
+                    </p>
+                    <Link to='/signup' className="btn">Start Writing</Link>
+                </article>
+                <div class="hero-img">
+                    <img src="/images/hero.png" alt=""/>
+                </div>
+            </section>
+    )
+}
+
+export default Home
